2395ffacf1970588f091ac7cf3afb374facbff05
Tomas Ferreyra <tomasl.ferreyra@hotmail.com>
2022-08-24 15:59:57 -0300

    commit navbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <NavBar />
+      <header className="App-header">       
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Proximamente
         </p>
         <a
           className="App-link"
---
+++ src/components/NavBar.js
@@ -0,0 +1,17 @@
+
+const NavBar = () => {
+    return (
+        <div className="navBar">
+      
+      <ul>
+          <a href='index.html'><img src="./logocircu.png" className="logoCircu" alt="logo" /></a>
+          <li><a href='index.html'>Inicio</a></li>
+          <li><a href='index.html'>Productos</a></li>
+          <li><a href='index.html'>Nosotros</a></li>
+      </ul>
+      
+      </div>
+    )
+}
+
+export default NavBar;
