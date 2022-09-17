8f4dcec4c273adf969995f4dc1f486a9e8f82b5c
Tomas Ferreyra <tomasl.ferreyra@hotmail.com>
2022-09-17 00:29:28 -0300

    commit

--- src/components/ItemListContainer/Item/Item.js
+++ src/components/ItemListContainer/Item/Item.js
@@ -1,16 +1,21 @@
 import './Item.css'
+import { Link } from 'react-router-dom';
 
 
-
-const Item = ({name,price,img}) => {
+const Item = ({id,name,price,img}) => {
   return (
     <div>  
+       <Link  to={'/detail/' + id }
+        style={{textDecoration : 'none', color: 'black' }} >
         <div className='card' >
+        
             <img src={img} alt="imagen" />
             <h2>{name}</h2>
             <h5>{price} </h5>
+        
             
         </div>
+        </Link>
     </div>
   )
 }
--- src/components/ItemListContainer/ItemList/ItemList.js
+++ src/components/ItemListContainer/ItemList/ItemList.js
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+
 import Item from "../Item/Item";
 import './ItemList.css';
 
@@ -8,10 +8,8 @@ const ItemList = ({list}) => {
     
     <div className="tarjeta">
         {list.map((product) => (
-        <Link key={product.id}  to={'/detail/' + product.id }
-        style={{textDecoration : 'none', color: 'black' }} > 
-        <Item   name={product.name} price= {product.price} img= {product.img} category = {product.category} />
-        </Link>))}   
+        <Item key={product.id} id={product.id}  name={product.name} price= {product.price} img= {product.img} category = {product.category} />
+      ))}   
     </div>
     
   );
