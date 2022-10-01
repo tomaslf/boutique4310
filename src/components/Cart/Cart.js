f3daf8f564523fc983555bcc7eb7b1c73bdba08b
Tomas Ferreyra <tomasl.ferreyra@hotmail.com>
2022-09-30 23:47:24 -0300

    Firebase II

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,12 +2,43 @@ import './Cart.css'
 import { useContext } from 'react';
 import { CartContext } from "../../context/CartContext";
 import { Button ,Alert } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
+import {getFirestore, collection, addDoc } from 'firebase/firestore';
+import moment from 'moment';
+import swal from 'sweetalert'
 
 const rutaInicial = '../images/'
 const Cart = () => {
 
     const {cart , removeItem, clear} = useContext(CartContext);
+    const total = cart.reduce((previousValue, currentValue)=> previousValue + currentValue.price * currentValue.cantidad,0 )
+    const createOrder = () =>{
+        const db = getFirestore();
+        clear();
+        
+        const orden = {
+            user: {
+                name: "Tomas",
+                email: "tomas@hotmail.com",
+                phone: "1158550197"
+            },
+            detalle:cart.map((item) => ({
+              name: item.name,
+              price: item.price,
+              cantidad: item.cantidad,
+            })),
+            date: moment().subtract(10, 'days').calendar(),
+            total: total,
+        };
+    const query = collection(db, "orders");
+    addDoc(query, orden)
+    .then(({id}) => console.log(id), swal(
+      'Gracias por tu compra!',
+      'Te enviaremos un mail con la factura',
+      'success'
+    ))
+    .catch(() => alert("Hubo un error al procesar tu compra"))
+    };
 
   return (
     <div>
@@ -36,9 +67,10 @@ const Cart = () => {
                             </div>  
                 </div>     
             ))}
+                <div className='totalCompra'> Total ${total} </div>
                 <div className='botonesFinales'>
                     <Button className='vaciar' variant='danger' onClick={()=> clear()} >Vaciar Carrito</Button>
-                    <Button className='finalizar' variant='success'  >Finalizar Compra</Button>
+                    <NavLink to={'/'}><Button className='finalizar' variant='success' onClick={createOrder}  >Finalizar Compra</Button></NavLink>
                 </div>    
             </>
             )};     
